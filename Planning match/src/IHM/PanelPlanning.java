53fef3b2da1a1a2d907ded27f23ca6b5d9f68c24
Julien-182 <julien.ducrot.mess@gmail.com>
2016-01-06 17:07:06 +0100

    modification + ajout de méthodes
    - ajout de getDateString() pour obtenir une date 'littéraire'

--- Planning match/src/IHM/PanelPlanning.java
+++ Planning match/src/IHM/PanelPlanning.java
@@ -27,19 +27,130 @@ public class PanelPlanning extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        labelTitre = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+
+        setBackground(new java.awt.Color(0, 0, 0));
+
+        labelTitre.setBackground(new java.awt.Color(0, 0, 0));
+        labelTitre.setFont(new java.awt.Font("Eras Bold ITC", 0, 36)); // NOI18N
+        labelTitre.setForeground(new java.awt.Color(255, 255, 255));
+        labelTitre.setText("Planning des matchs");
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Date", "Horaire", "Participantsl", "Court"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, true, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane1.setViewportView(jTable1);
+
+        jButton1.setText("Editer");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jButton2.setText("Ajouter");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        jButton3.setText("Supprimer");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1000, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(337, 337, 337)
+                .addComponent(labelTitre)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(39, 39, 39)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 728, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 52, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jButton1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 129, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 129, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(52, 52, 52))
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                    .addContainerGap(817, Short.MAX_VALUE)
+                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 129, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGap(54, 54, 54)))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 700, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(31, 31, 31)
+                .addComponent(labelTitre)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(185, 185, 185)
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(29, 29, 29)
+                        .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(31, 31, 31)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 542, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(54, Short.MAX_VALUE))
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(174, 174, 174)
+                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addContainerGap(472, Short.MAX_VALUE)))
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton3ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JLabel labelTitre;
     // End of variables declaration//GEN-END:variables
 }
--- Planning match/src/planning/match/match/Match.java
+++ Planning match/src/planning/match/match/Match.java
@@ -13,7 +13,7 @@ public class Match{
     private String creneau;
     private String categorie_match;
     private String tour;
-    private Court court;
+    private int id_court;
     
     private Arbitre arbitreChaise, arbitreFilet;
     private List<Arbitre> arbitresLigne = new ArrayList<>();
@@ -23,17 +23,18 @@ public class Match{
     private Participant participant2;
     
     
-    public Match(int id_match, Date date, String creneau, String categorie, String tour){
+    public Match(int id_match, Date date, String creneau, String categorie, String tour, int id_court){
         this.date = date;   
         this.creneau = creneau;
         this.categorie_match = categorie;
         this.tour = tour;
+        this.id_court = id_court;
     }
 
     @Override
     public String toString() {
         return "Match{" + "id_match=" + id_match + ", date=" + date + ", creneau=" + creneau + ", categorie_match=" 
-                + categorie_match + ", tour=" + tour + ", court=" + court + ", arbitreChaise=" + arbitreChaise 
+                + categorie_match + ", tour=" + tour + ", court=" + id_court + ", arbitreChaise=" + arbitreChaise 
                 + ", arbitreFilet=" + arbitreFilet + ", arbitresLigne=" + arbitresLigne + ", ramasseursDroite=" 
                 + ramasseursDroite + ", ramasseursGauche=" + ramasseursGauche + ", participant1=" + participant1 
                 + ", participant2=" + participant2 + '}';
@@ -41,6 +42,51 @@ public class Match{
     
     public int getId_match() {
         return id_match;
+    }
+    
+    public String getDateString(){
+  
+        String jour_s,mois_s,annee_s;
+        String date_s = date.toString();
+        //System.out.println(date_s);
+        //2016-01-05
+        annee_s = date_s.substring(0, 4);
+        mois_s = date_s.substring(5, 7);
+        jour_s = date_s.substring(8,10);
+        /*
+        System.out.println("jour " + jour_s);
+        System.out.println("mois " + mois_s);
+        System.out.println("annee " + annee_s);
+        */
+        switch(mois_s){
+            case "01": mois_s = "Janvier";
+                    break;
+            case "02" : mois_s = "Février";
+                    break;
+            case "03": mois_s = "Mars";
+                    break;
+            case "04" : mois_s = "Avril";
+                    break;
+            case "05": mois_s = "Mai";
+                    break;
+            case "06" : mois_s = "Juin";
+                    break;
+            case "07": mois_s = "Juillet";
+                    break;
+            case "08" : mois_s = "Août";
+                    break;   
+            case "09": mois_s = "Septembre";
+                    break;
+            case "10" : mois_s = "Octobre";
+                    break;
+            case "11": mois_s = "Novembre";
+                    break;
+            case "12" : mois_s = "Décembre";
+                    break;
+            default : mois_s = "Problème :/";
+                    break;
+        }        
+        return jour_s + " " + mois_s + " " + annee_s;
     }
 
     public Date getDate() {
